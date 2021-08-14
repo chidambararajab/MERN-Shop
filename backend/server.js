3949eea577de4d2522f05113f87b6875dfe6c252
Chidambararajab <chidambararaja97@gmail.com>
2021-08-14 14:36:48 +0530

    Removed comment.

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import products from './data/products.js';
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
 // To get every environment variable to this folder.
-// Another Line of code.
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Api is running...');
+app.get("/", (req, res) => {
+  res.send("Api is running...");
 });
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-app.get('/api/products/:id', (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
