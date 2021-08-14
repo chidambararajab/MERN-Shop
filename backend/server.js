030882d1d9865b61c72a880a7331d5378632cb64
Chidambararajab <chidambararaja97@gmail.com>
2021-08-14 21:49:16 +0530

    Create a productRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 
+import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
 
 // To get every environment variable to this folder.
 dotenv.config();
@@ -15,14 +16,7 @@ app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
