89a2f85b80e88391c67175557b66363d373076a6
Chidambararaja Boominathan <cboominathan@connectall.com>
2021-08-14 14:33:56 +0530

    Added comment in serverjs For checking pull in github.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
 
+// To get every environment variable to this folder.
 dotenv.config();
 
 const app = express();
