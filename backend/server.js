2a671cbc9b5649119fb90c2066e9185d20bdc08d
Chidambararaja Boominathan <cboominathan@connectall.com>
2021-08-14 14:34:54 +0530

    Another comment.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import products from './data/products.js';
 
 // To get every environment variable to this folder.
+// Another Line of code.
 dotenv.config();
 
 const app = express();
