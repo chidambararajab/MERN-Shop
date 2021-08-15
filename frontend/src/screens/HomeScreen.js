d120b7d17db59d466eaf68296713f8e80f54429b
Chidambararajab <chidambararaja97@gmail.com>
2021-08-15 15:01:37 +0530

    useDispatch & useSelector Added custom error handler

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 
-import Product from '../components/Product';
+import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products/');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
-      <h1 style={{ paddingTop: '60px' }}>latest products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={2}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      <h1 style={{ paddingTop: "60px" }}>latest products</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={2}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers({});
+import { productListReducer } from "./reducers/productReducers";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
 const initialState = {};
 const middleware = [thunk];
