49eede6cb5c3e6a3565b7d7554a03e2388406863
Chidambararajab <chidambararaja97@gmail.com>
2021-08-15 17:37:15 +0530

    Product details reducer & action added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 import Product from "../components/Product";
 import { listProducts } from "../actions/productActions";
 
@@ -19,9 +21,9 @@ const HomeScreen = () => {
     <>
       <h1 style={{ paddingTop: "60px" }}>latest products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import {
   Row,
   Col,
@@ -10,20 +9,23 @@ import {
   Button,
   Container,
 } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listProductDetails } from "../actions/productActions";
 
 // match prop helps to find the id in url.
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`);
-      setProduct(data);
-    };
-    fetchProduct();
-  }, [match.params.id]);
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
   return (
     <div
@@ -34,102 +36,110 @@ const ProductScreen = ({ match }) => {
       <Link className="btn btn-light my-3" to="/">
         {`<---`}
       </Link>
-      <Container className="m-2">
-        <Row>
-          <Col md={3} style={{ margin: "3% 0" }}>
-            <Image
-              src={product.image}
-              alt={product.name}
-              style={{ maxHeight: "500px", maxWidth: "250px" }}
-            />
-          </Col>
-          <Col md={5} style={{ margin: "3% 0" }}>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <h6
-                  style={{
-                    padding: 0,
-                    margin: 1,
-                    color: "gray",
-                  }}
-                >
-                  {`${product.brand} `}
-                </h6>
-                <h5>{product.name}</h5>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Rating
-                  value={product.rating}
-                  text={` ${product.numReviews} reviews`}
-                />
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <h6>₹ {product.price}</h6>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <strong>Product Description: </strong>
-                {product.description}
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-          <Col md={3} style={{ margin: "3% 0" }}>
-            <Card>
-              <ListGroup>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Container className="m-2">
+          <Row>
+            <Col md={3} style={{ margin: "3% 0" }}>
+              <Image
+                src={product.image}
+                alt={product.name}
+                style={{ maxHeight: "500px", maxWidth: "250px" }}
+              />
+            </Col>
+            <Col md={5} style={{ margin: "3% 0" }}>
+              <ListGroup variant="flush">
                 <ListGroup.Item>
-                  <Row>
-                    <Col>
-                      <span>Price : </span>
-                    </Col>
-                    <Col>
-                      <strong>₹{product.price}</strong>
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>
-                      <span>Status : </span>
-                    </Col>
-                    <Col>
-                      <strong>
-                        {product.countInStock > 0 ? (
-                          <span style={{ color: "#007600" }}>In Stock</span>
-                        ) : (
-                          <span style={{ color: "#B12704" }}>Out Of Stock</span>
-                        )}
-                      </strong>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <span
-                      className="mt-2"
-                      style={{ color: "#272727", fontSize: "12px" }}
-                    >
-                      Sold by @{product.brand}
-                    </span>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Button
-                    className="btn-block"
-                    disabled={product.countInStock === 0}
-                    type="button"
+                  <h6
                     style={{
-                      color: "#f7f7f7",
-                      backgroundColor: "#272727",
-                      borderRadius: 5,
-                      width: "100%",
-                      borderColor: "#272727",
+                      padding: 0,
+                      margin: 1,
+                      color: "gray",
                     }}
                   >
-                    Add To Cart
-                  </Button>
+                    {`${product.brand} `}
+                  </h6>
+                  <h5>{product.name}</h5>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    value={product.rating}
+                    text={` ${product.numReviews} reviews`}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <h6>₹ {product.price}</h6>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Product Description: </strong>
+                  {product.description}
                 </ListGroup.Item>
               </ListGroup>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+            <Col md={3} style={{ margin: "3% 0" }}>
+              <Card>
+                <ListGroup>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>
+                        <span>Price : </span>
+                      </Col>
+                      <Col>
+                        <strong>₹{product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>
+                        <span>Status : </span>
+                      </Col>
+                      <Col>
+                        <strong>
+                          {product.countInStock > 0 ? (
+                            <span style={{ color: "#007600" }}>In Stock</span>
+                          ) : (
+                            <span style={{ color: "#B12704" }}>
+                              Out Of Stock
+                            </span>
+                          )}
+                        </strong>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <span
+                        className="mt-2"
+                        style={{ color: "#272727", fontSize: "12px" }}
+                      >
+                        Sold by @{product.brand}
+                      </span>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block"
+                      disabled={product.countInStock === 0}
+                      type="button"
+                      style={{
+                        color: "#f7f7f7",
+                        backgroundColor: "#272727",
+                        borderRadius: 5,
+                        width: "100%",
+                        borderColor: "#272727",
+                      }}
+                    >
+                      Add To Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      )}
     </div>
   );
 };
